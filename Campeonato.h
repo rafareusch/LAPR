12cde664741334f6739f790468983d3efe61af7d
rafareusch <rafa_reusch@hotmail.com>
2015-09-24 15:51:16 -0300

    Create Campeonato.h

---
+++ Campeonato.h
@@ -0,0 +1,18 @@
+#ifndef CAMPEONATO_H
+#define CAMPEONATO_H
+#include<iostream>
+
+#include "Clube.h"
+using namespace std;
+
+
+class Campeonato
+{
+//	Clube Clubes[20];
+	public:
+		Campeonato();
+		void LeArquivo();
+	
+};
+
+#endif

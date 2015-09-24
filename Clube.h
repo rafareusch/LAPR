82cf4ccbdf6b6b61f234b3bdf5f322d870b6c74f
rafareusch <rafa_reusch@hotmail.com>
2015-09-24 15:51:33 -0300

    Create Clube.h

---
+++ Clube.h
@@ -0,0 +1,18 @@
+#ifndef CLUBE_H
+#define CLUBE_H
+#include<iostream>
+
+#include<string.h>
+using namespace std;
+
+
+class Clube
+{
+		string nome;
+		int pontos,vitorias,saldo,gols;
+		
+	public:
+		Clube();
+
+};
+#endif

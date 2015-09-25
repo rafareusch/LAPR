916c28257a92064e48f5fb1450e678cb9fec2cf7
rafareusch <rafa_reusch@hotmail.com>
2015-09-25 19:49:58 -0300

    Update Campeonato.cpp

--- Campeonato.cpp
+++ Campeonato.cpp
@@ -11,25 +11,59 @@ Campeonato::Campeonato()
 	cout<< "teste construtor";
 }
 
-void Campeonato::LeArquivo(){
+void Campeonato::LeArquivo(){// string 19 = primeiro time
+    int i=0,gol1,gol2;
     ifstream arq;
-    string teste, linha;
+    string linha, time1,time2;
 
     arq.open("Jogos.txt", ios::in);
 
-    if (!arq)
-    {
+    if (!arq){
       cout << "Problemas na abertura do arquivo" << endl;
       return;
     }
 
-    while (!arq.eof())
-   {
+    while (!arq.eof()){
         getline(arq,linha,' ');
-        cout << linha << endl;
-
+       // cout << linha << "/";
+        if (i == 18){
+            time1 = linha;
+          //  cout << time1 << endl;
+          //  cout << "i =" << i <<"  ";
+        }
+        if ( i == 22){
+            gol1 = atoi(linha.c_str());
+           // cout << gol1 << endl;
+           // cout << "i =" << i <<"  ";
+        }
+        if ( i == 30){
+            gol2 = atoi(linha.c_str());
+           // cout << gol2 << endl;
+           // cout << "i =" << i <<"  ";
+        }
+        if ( i == 34){
+            time2 = linha;
+           // cout << time2 << endl;
+           //cout << "i =" << i <<"  ";
+            Vencedor(time1,time2,gol1,gol2);
+            i=0;
+        }
+        i++;
    }
-     arq.close();
+    arq.close();
+}
 
+
+void Campeonato::Vencedor(string time1, string time2, int gol1, int gol2){
+    if( gol1 > gol2 ){
+        cout << "1Vencedor: " << time1 << endl;
+    }
+    if( gol1 < gol2 ){
+        cout << "2Vencedor: " << time2 << endl;
+    }
+    if( gol1 == gol2 ){
+        cout << "Empate!" << endl;
+    }
 
 }
+

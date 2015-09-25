683d9f7b709b822761336cbef35600c0fb3c6cce
rafareusch <rafa_reusch@hotmail.com>
2015-09-25 20:27:21 -0300

    Update Campeonato.cpp

--- Campeonato.cpp
+++ Campeonato.cpp
@@ -12,9 +12,9 @@ Campeonato::Campeonato()
 }
 
 void Campeonato::LeArquivo(){// string 19 = primeiro time
-    int i=0,gol1,gol2;
+    int i=0,gol1,gol2,ex=0;
     ifstream arq;
-    string linha, time1,time2;
+    string linha, time1,time2,lixo;
 
     arq.open("Jogos.txt", ios::in);
 
@@ -25,31 +25,57 @@ void Campeonato::LeArquivo(){// string 19 = primeiro time
 
     while (!arq.eof()){
         getline(arq,linha,' ');
+        //cout<<"i= "<<i<<"//"<<linha<<endl;
        // cout << linha << "/";
         if (i == 18){
+               // cout<<"//entro 1 if"<<endl;
+                if (linha == "Sao"){
+                    linha = "Sao Paulo";
+                    ex=1;
+                }
+                if (linha == "Ponte"){
+                    linha = "Ponte Preta";
+                    ex=1;
+                }
             time1 = linha;
           //  cout << time1 << endl;
           //  cout << "i =" << i <<"  ";
         }
-        if ( i == 22){
+        if ( i == (22 + ex)){
+
             gol1 = atoi(linha.c_str());
            // cout << gol1 << endl;
            // cout << "i =" << i <<"  ";
         }
-        if ( i == 30){
+        if ( i == (30 + ex)){
+
             gol2 = atoi(linha.c_str());
            // cout << gol2 << endl;
            // cout << "i =" << i <<"  ";
         }
-        if ( i == 34){
+        if ( i == (34 + ex)){
+
+
+            if (linha == "Sao"){
+                    linha = "Sao Paulo";
+                    getline(arq,lixo,' ');
+            }
+            if (linha == "Ponte"){
+                    linha = "Ponte Preta";
+                    getline(arq,lixo,' ');
+            }
             time2 = linha;
            // cout << time2 << endl;
            //cout << "i =" << i <<"  ";
             Vencedor(time1,time2,gol1,gol2);
             i=0;
+            ex=0;
         }
+
         i++;
+
    }
+
     arq.close();
 }
 

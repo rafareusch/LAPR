c3166f9fc1e953e9032ac8a32825cb59548a0dcf
rafareusch <rafa_reusch@hotmail.com>
2015-09-27 00:13:56 -0300

    Update Clube.h

--- Clube.h
+++ Clube.h
@@ -9,10 +9,24 @@ using namespace std;
 class Clube
 {
 		string nome;
-		int pontos,vitorias,saldo,gols;
-		
+		int jogos,pontos,vitorias,empates,derrotas,saldo,gols,golstomados;
+        bool estado;
 	public:
 		Clube();
+		void AdicionarPontos(int p);
+		void AdicionarVitoria();
+		void ModificarSaldo(int gol);
+		void AdicionarGol(int gol);
+		void AdicionarGolTomado(int gol);
+		void AdicionarJogo();
+		void AdicionarDerrota();
+		void AdicionarEmpate();
+		void Exibir();
+		void TrocaEstado();
+		bool GetEstado();
+
+
 
 };
 #endif
+

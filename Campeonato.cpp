f18616c7714cc9ee9bf7693c64e54aac9a04136e
rafareusch <rafa_reusch@hotmail.com>
2015-09-27 00:12:59 -0300

    Update Campeonato.cpp

--- Campeonato.cpp
+++ Campeonato.cpp
@@ -1,14 +1,15 @@
 #include "Campeonato.h"
 #include<iostream>
 #include<fstream>
-#include<string.h>
-
-using std::ifstream;
+#include<string>
+#include<stdlib.h>
 using namespace std;
+using std::ifstream;
+
 
 Campeonato::Campeonato()
 {
-	cout<< "teste construtor";
+
 }
 
 void Campeonato::LeArquivo(){// string 19 = primeiro time
@@ -22,67 +23,48 @@ void Campeonato::LeArquivo(){// string 19 = primeiro time
       cout << "Problemas na abertura do arquivo" << endl;
       return;
     }
-
     while (!arq.eof()){
         getline(arq,linha,' ');
-        //cout<<"i= "<<i<<"//"<<linha<<endl;
-       // cout << linha << "/";
         if (i == 18){
-               // cout<<"//entro 1 if"<<endl;
-                if (linha == "Sao"){
-                    linha = "Sao Paulo";
-                    ex=1;
-                }
-                if (linha == "Ponte"){
-                    linha = "Ponte Preta";
-                    ex=1;
-                }
-            time1 = linha;
-          //  cout << time1 << endl;
-          //  cout << "i =" << i <<"  ";
-        }
-        if ( i == (22 + ex)){
-
-            gol1 = atoi(linha.c_str());
-           // cout << gol1 << endl;
-           // cout << "i =" << i <<"  ";
-        }
-        if ( i == (30 + ex)){
-
-            gol2 = atoi(linha.c_str());
-           // cout << gol2 << endl;
-           // cout << "i =" << i <<"  ";
-        }
-        if ( i == (34 + ex)){
-
-
             if (linha == "Sao"){
-                    linha = "Sao Paulo";
-                    getline(arq,lixo,' ');
+                linha = "Sao Paulo";
+                ex=1;
             }
             if (linha == "Ponte"){
-                    linha = "Ponte Preta";
-                    getline(arq,lixo,' ');
+                linha = "Ponte Preta";
+                ex=1;
+            }
+            time1 = linha;
+        }
+        if ( i == (22 + ex)){
+            gol1 = atoi(linha.c_str());
+        }
+        if ( i == (30 + ex)){
+            gol2 = atoi(linha.c_str());
+        }
+        if ( i == (34 + ex)){
+            if (linha == "Sao"){
+                linha = "Sao Paulo";
+                getline(arq,lixo,' ');
+            }
+            if (linha == "Ponte"){
+                linha = "Ponte Preta";
+                getline(arq,lixo,' ');
             }
             time2 = linha;
-           // cout << time2 << endl;
-           //cout << "i =" << i <<"  ";
             Vencedor(time1,time2,gol1,gol2);
             i=0;
             ex=0;
         }
-
         i++;
-
    }
-
     arq.close();
 }
-
 
 void Campeonato::Vencedor(string time1, string time2, int gol1, int gol2){
     if( gol1 > gol2 ){
         cout << "1Vencedor: " << time1 << endl;
+        Atribuir(time1,gol1,gol2,1);
     }
     if( gol1 < gol2 ){
         cout << "2Vencedor: " << time2 << endl;
@@ -91,5 +73,48 @@ void Campeonato::Vencedor(string time1, string time2, int gol1, int gol2){
         cout << "Empate!" << endl;
     }
 
+}
+
+void Campeonato::Atribuir(string nome,int golfeito, int goltomado, int vitoria){
+    int i = Pesquisar(nome), saldo = (golfeito-goltomado);
+
+    if (vitoria == 1){
+        Clubes[i].AdicionarGol(golfeito);
+        Clubes[i].AdicionarPontos(3);
+        Clubes[i].ModificarSaldo(saldo);
+        Clubes[i].AdicionarVitoria();
+        Clubes[i].AdicionarJogo();
+        Clubes[i].AdicionarGolTomado(goltomado);
+    return;
+    }
+    if (vitoria == 0){
+        Clubes[i].AdicionarEmpate();
+        Clubes[i].AdicionarGol(golfeito);
+        Clubes[i].AdicionarGolTomado(goltomado);
+        Clubes[i].ModificarSaldo(saldo);
+        Clubes[i].AdicionarJogo();
+        Clubes[i].AdicionarPontos(1);
+        return;
+    }
+    if (vitoria == -1){
+        Clubes[i].AdicionarGol(golfeito);
+        Clubes[i].ModificarSaldo(saldo);
+        Clubes[i].AdicionarDerrota();
+        Clubes[i].AdicionarJogo();
+        Clubes[i].AdicionarGolTomado(goltomado);
+        return;
+    }
+
+}
+int Campeonato::Pesquisar(string nome){
+    int i=0;
+    bool found=false;
+
+    while (!found){
+        if (!Clubes[i].GetEstado())
+            return i;
+        i++;
+    }
+    return -1;
 }
 

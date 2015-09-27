16402382ed58ed0822c3b31d7bca2182ecb3996d
rafareusch <rafa_reusch@hotmail.com>
2015-09-27 00:13:18 -0300

    Update Campeonato.h

--- Campeonato.h
+++ Campeonato.h
@@ -8,11 +8,13 @@ using namespace std;
 
 class Campeonato
 {
-//	Clube Clubes[20];
+	Clube Clubes[20];
 	public:
 		Campeonato();
 		void LeArquivo();
 		void Vencedor(string time1, string time2, int gol1, int gol2);
+		int Pesquisar(string nome);
+		void Atribuir(string nome,int golfeito, int goltomado, int vitoria);
 
 };
 

6b4c8c07ad7e2757f31bf684f3e3fa5c14f7ced1
rafareusch <rafa_reusch@hotmail.com>
2015-09-27 00:13:43 -0300

    Update Clube.cpp

--- Clube.cpp
+++ Clube.cpp
@@ -4,5 +4,60 @@ using namespace std;
 
 Clube::Clube()
 {
+    pontos = 0;
+    vitorias = 0;
+    saldo = 0;
+    gols = 0;
+    estado=false;
 }
+
+void Clube::AdicionarJogo(){
+    jogos += 1;
+}
+
+void Clube::AdicionarPontos(int p){
+    pontos += p;
+}
+
+void Clube::AdicionarVitoria(){
+    vitorias += 1;
+}
+
+void Clube::AdicionarEmpate(){
+    empates += 1;
+}
+
+void Clube::AdicionarDerrota(){
+    derrotas += 1;
+}
+
+void Clube::ModificarSaldo(int s){
+    saldo += s;
+}
+
+void Clube::AdicionarGol(int gol){
+    gols += gol;
+}
+
+void Clube::AdicionarGolTomado(int gol){
+    golstomados += gol;
+}
+
+void Clube::Exibir(){
+    cout << "Time: " << nome << endl;
+    cout << "Vitorias: " << vitorias << endl;
+    cout << "Saldo: " << saldo << endl;
+    cout << "Gols: " << gols << endl;
+}
+
+void Clube::TrocaEstado(){
+    estado = !estado;
+}
+
+bool Clube::GetEstado(){
+    return estado;
+}
+
+
+
 

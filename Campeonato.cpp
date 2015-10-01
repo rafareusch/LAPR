24bf1b27cd339619e24d8ee27fa759ccbf5f36ce
rafareusch <rafa_reusch@hotmail.com>
2015-10-01 16:12:04 -0300

    Update Campeonato.cpp

--- Campeonato.cpp
+++ Campeonato.cpp
@@ -9,55 +9,31 @@ using std::ifstream;
 
 Campeonato::Campeonato()
 {
+    Clube();
 
 }
 
-void Campeonato::LeArquivo(){// string 19 = primeiro time
-    int i=0,gol1,gol2,ex=0;
+void Campeonato::LeArquivo(){
+    int i=1,gol1,gol2;
     ifstream arq;
-    string linha, time1,time2,lixo;
-
+    string linha,time1,time2;
     arq.open("Jogos.txt", ios::in);
-
-    if (!arq){
-      cout << "Problemas na abertura do arquivo" << endl;
-      return;
-    }
     while (!arq.eof()){
-        getline(arq,linha,' ');
-        if (i == 18){
-            if (linha == "Sao"){
-                linha = "Sao Paulo";
-                ex=1;
-            }
-            if (linha == "Ponte"){
-                linha = "Ponte Preta";
-                ex=1;
-            }
+        getline(arq,linha,'\t');
+        if (i==5)
             time1 = linha;
-        }
-        if ( i == (22 + ex)){
+        if (i==6)
             gol1 = atoi(linha.c_str());
-        }
-        if ( i == (30 + ex)){
+        if (i==8)
             gol2 = atoi(linha.c_str());
-        }
-        if ( i == (34 + ex)){
-            if (linha == "Sao"){
-                linha = "Sao Paulo";
-                getline(arq,lixo,' ');
-            }
-            if (linha == "Ponte"){
-                linha = "Ponte Preta";
-                getline(arq,lixo,' ');
-            }
+        if (i==9)
             time2 = linha;
+        if (i==11){
             Vencedor(time1,time2,gol1,gol2);
             i=0;
-            ex=0;
         }
         i++;
-   }
+    }
     arq.close();
 }
 
@@ -65,56 +41,56 @@ void Campeonato::Vencedor(string time1, string time2, int gol1, int gol2){
     if( gol1 > gol2 ){
         cout << "1Vencedor: " << time1 << endl;
         Atribuir(time1,gol1,gol2,1);
+        Atribuir(time2,gol2,gol1,-1);
     }
     if( gol1 < gol2 ){
         cout << "2Vencedor: " << time2 << endl;
+        Atribuir(time2,gol2,gol1,1);
+        Atribuir(time1,gol1,gol2,-1);
     }
     if( gol1 == gol2 ){
+        Atribuir(time1,gol1,gol2,0);
+        Atribuir(time2,gol2,gol1,0);
         cout << "Empate!" << endl;
     }
 
 }
 
 void Campeonato::Atribuir(string nome,int golfeito, int goltomado, int vitoria){
-    int i = Pesquisar(nome), saldo = (golfeito-goltomado);
+    int i;
+    i = Pesquisar(nome);
+    if (vitoria == 1)
+        Clubes[i].AdicionarJogo(nome,golfeito,goltomado,1);
+    if (vitoria == 0)
+        Clubes[i].AdicionarJogo(nome,golfeito,goltomado,0);
+    if (vitoria == -1)
+        Clubes[i].AdicionarJogo(nome,golfeito,goltomado,-1);
 
-    if (vitoria == 1){
-        Clubes[i].AdicionarGol(golfeito);
-        Clubes[i].AdicionarPontos(3);
-        Clubes[i].ModificarSaldo(saldo);
-        Clubes[i].AdicionarVitoria();
-        Clubes[i].AdicionarJogo();
-        Clubes[i].AdicionarGolTomado(goltomado);
     return;
-    }
-    if (vitoria == 0){
-        Clubes[i].AdicionarEmpate();
-        Clubes[i].AdicionarGol(golfeito);
-        Clubes[i].AdicionarGolTomado(goltomado);
-        Clubes[i].ModificarSaldo(saldo);
-        Clubes[i].AdicionarJogo();
-        Clubes[i].AdicionarPontos(1);
-        return;
-    }
-    if (vitoria == -1){
-        Clubes[i].AdicionarGol(golfeito);
-        Clubes[i].ModificarSaldo(saldo);
-        Clubes[i].AdicionarDerrota();
-        Clubes[i].AdicionarJogo();
-        Clubes[i].AdicionarGolTomado(goltomado);
-        return;
-    }
-
 }
 int Campeonato::Pesquisar(string nome){
     int i=0;
-    bool found=false;
 
-    while (!found){
-        if (!Clubes[i].GetEstado())
+    while (i<20){
+        if (nome == Clubes[i].GetNome() || Clubes[i].GetEstado() == false){
+            cout << i << " retornado" << endl;
+            Clubes[i].TrocaEstado();
             return i;
+        }
         i++;
     }
-    return -1;
+    cout << "Sem espaço"; // nunca deve chegar aqui
+    return 0;
 }
+
+void Campeonato::Exibe(){
+    int i=0;
+    while (i<20){
+        Clubes[i].Exibir();
+        cout << endl;
+        i++;
+    }
+    return;
+}
+
 

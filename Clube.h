4ba151afd7247725e44fb86312f62b5da5a89cb1
rafareusch <rafa_reusch@hotmail.com>
2015-10-04 03:41:51 -0300

    Update Clube.h

--- Clube.h
+++ Clube.h
@@ -10,7 +10,7 @@ class Clube
 {
 		string nome;
 		int jogos,pontos,vitorias,empates,derrotas,saldo,gols,golstomados;
-        bool estado;
+        	bool estado;
 	public:
 		Clube();
 		void AdicionarJogo(string time,int gols,int golst, int controle);
@@ -18,10 +18,23 @@ class Clube
 		void TrocaEstado();
 		bool GetEstado();
 		string GetNome();
-
-
-
+		int GetPontos();
+		void SetNome(string n);
+		int GetJogos();
+		int GetVitorias();
+	        int GetEmpates();
+		int GetDerrota();
+		int GetSaldo();
+		int GetGols()
+		int GetGolsTomados();
+        	void SetJogos(int x);
+        	void SetVitorias(int x);
+        	void SetEmpates(int x);
+        	void SetDerrota(int x);
+        	void SetSaldo(int x);
+        	void SetGols(int x);
+        	void SetGolsTomados(int x);
+        	void SetPontos(int x);
 };
 #endif
-
 

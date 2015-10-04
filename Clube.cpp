767d717cd6d0b2fa1f4ee486e09db49c3f83e3e4
rafareusch <rafa_reusch@hotmail.com>
2015-10-04 03:40:11 -0300

    Update Clube.cpp

--- Clube.cpp
+++ Clube.cpp
@@ -16,9 +16,10 @@ Clube::Clube()
 }
 
 
+
 void Clube::AdicionarJogo(string time,int golsf,int golst, int controle){
-    saldo = saldo + (golsf - golst);
     nome = time;
+    saldo = saldo + (golsf - golst);
     jogos += 1;
     gols += golsf;
     golstomados += golst;
@@ -43,7 +44,7 @@ void Clube::Exibir(){
     cout << "Time: " << nome << endl;
     cout << "Pontos: " << pontos << endl;
     cout << "Jogos: " << jogos << endl;
-     cout << "Vitorias " << vitorias << endl;
+    cout << "Vitorias " << vitorias << endl;
     cout << "Empates:" << empates << endl;
     cout << "Derrotas: "  << derrotas << endl;
     cout << "Saldo: " << saldo << endl;
@@ -63,6 +64,57 @@ string Clube::GetNome(){
     return nome;
 }
 
+int Clube::GetPontos(){
+    return pontos;
+}
 
+void Clube::SetNome(string n){
+    nome = n;
+}
 
+int Clube::GetJogos(){
+    return jogos;
+}
+int Clube::GetVitorias(){
+    return vitorias;
+}
+int Clube::GetEmpates(){
+    return empates;
+}
+int Clube::GetDerrota(){
+    return derrotas;
+}
+int Clube::GetSaldo(){
+    return saldo;
+}
+int Clube::GetGols(){
+    return gols;
+}
+int Clube::GetGolsTomados(){
+    return golstomados;
+}
+void Clube::SetJogos(int x){
+    jogos = x;
+}
+void Clube::SetVitorias(int x){
+    vitorias = x;
+}
+void Clube::SetEmpates(int x){
+     empates = x;
+}
+void Clube::SetDerrota(int x){
+     derrotas = x;
+}
+void Clube::SetSaldo(int x){
+     saldo = x;
+}
+void Clube::SetGols(int x){
+     gols = x;
+}
+void Clube::SetGolsTomados(int x){
+    golstomados = x;
+}
+void Clube::SetPontos(int x){
+    pontos = x;
+}
 
